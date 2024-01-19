4b2543cd453915f55b994f36ccd13c91344057a3
soon <sudosoon@gmail.com>
2024-01-19 17:49:13 +0900

    dap update 2

--- plugins/dap.lua
+++ plugins/dap.lua
@@ -2,28 +2,31 @@ return {
   'mfussenegger/nvim-dap',
   dependencies = {
     { 'theHamsta/nvim-dap-virtual-text', config = true },
+    { 'mxsdev/nvim-dap-vscode-js' },
   },
   config = function()
     local dap = require 'dap'
-    require('dap').adapters["pwa-node"] = {
+    require('dap-vscode-js').setup({
+    debugger_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter',
+    debugger_cmd = { 'js-debug-adapter' },
+    adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+})
+    dap.adapters["pwa-node"] = {
       type = 'server',
       host = 'localhost',
       port = '5000',
       executable = {
-        command = 'js-debug-adapter', -- As I'm using mason, this will be in the path
+        command = 'js-debug-adapter',
         args = { '5000' },
       }
     }
-
     for _, language in ipairs { 'typescript', "javascript" } do
-      require('dap').configurations[language] = {
+      dap.configurations[language] = {
         {
           type = 'pwa-node',
           request = 'launch',
           name = 'Launch file',
-          runtimeArgs ='ts-node-dev',
           program = 'src/app.ts',
-          cwd = '/Users/sw02/soon/blitz/workspace/blitz-admin-api',
         },
       }
     end
