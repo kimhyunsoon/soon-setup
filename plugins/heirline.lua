c449ffea4b0aecfe89fcca684e6784ee92dbf81a
soon <sudosoon@gmail.com>
2024-01-24 08:58:27 +0900

    edit theme options

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -5,7 +5,7 @@ return {
       local status = require("astronvim.utils.status")
       opts.statusline = { -- statusline
         hl = { fg = "fg", bg = "bg" },
-        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.mode { mode_text = false }, -- add the mode text
         status.component.git_branch(),
         status.component.file_info { filetype = {}, filename = false, file_modified = false },
         status.component.git_diff(),
--- plugins/sonokai.lua
+++ plugins/sonokai.lua
@@ -1,16 +1,10 @@
 return {
   'sainnhe/sonokai',
   init = function()
+    vim.g.sonokai_menu_selection_backgrounc = 'red';
+    vim.g.sonokai_transparent_background = 2;
     vim.g.sonokai_style = 'shusia';
     vim.g.sonokai_disable_italic_comment = 1;
-    vim.g.sonokai_colors_override = {
-      bg_dim = {'#000000', 232},
-      bg0 = {'#000000', 235},
-      bg1 = {'#111111', 236},
-      bg2 = {'#111111', 236},
-      bg3 = {'#222222', 237},
-      bg4 = {'#222222', 237},
-    };
-  end,
+  end
 }
 
